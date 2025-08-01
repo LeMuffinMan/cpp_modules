3975fc0c36d609cc6d79e0bd1e4d727209eec611
muffin <muffin@arch.man>
2025-08-02 00:45:31 +0200

    try catch added

--- 00-Claude/ex06/src/main.cpp
+++ 00-Claude/ex06/src/main.cpp
@@ -7,13 +7,24 @@ int main(void)
     ConfigParser config("config.txt");
     if (config.load())
     {
-        std::string host = config.getString("db_host");
-        int port = config.getInt("db_port");
-        std::cout << "port = " << port << std::endl;
-        bool debug = config.getBool("debug_mode");
-        std::cout << "debug = " << debug << std::endl;
-        double timeout = config.getDouble("timeout");
-        std::cout << "timeout = " << timeout << std::endl;
-    }
+        try
+        {
+            std::string host = config.getString("db_host");
+            int port = config.getInt("db_port");
+            bool debug = config.getBool("debug_mode");
+            double timeout = config.getDouble("timeout");
+
+            std::cout << "Host: " << host << std::endl;
+            std::cout << "Port: " << port << std::endl;
+            std::cout << "Debug: " << (debug ? "true" : "false") << std::endl;
+            std::cout << "Timeout: " << timeout << std::endl;
+            std::cout << "Host as int: " << config.getInt("db_host") << std::endl;
+
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Erreur: " << e.what() << std::endl;
+        }
     return 0;
+    }
 }
