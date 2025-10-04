73354854a1173e7e430e6a74e76a14f4a7ff44b7
Olivier Elleaume <oelleaum@z3r3p3.42lyon.fr>
2025-10-04 16:32:36 +0200

    =0 deleted for the destructor, added on makeSound for AAnimal as on WrongAnimal

--- 04/ex02/include/AAnimal.hpp
+++ 04/ex02/include/AAnimal.hpp
@@ -6,16 +6,16 @@
 
 class AAnimal
 {
-public:
-    AAnimal();
-    AAnimal(const AAnimal& other);
-    AAnimal& operator=(const AAnimal& other);
-    virtual ~AAnimal() = 0;
-    
-    std::string getType() const;
-    virtual void makeSound() const;
-protected:
-    std::string _type;
+    public:
+        AAnimal();
+        AAnimal(const AAnimal& other);
+        AAnimal& operator=(const AAnimal& other);
+        virtual ~AAnimal();
+        
+        virtual void makeSound() const = 0;
+        std::string getType() const;
+    protected:
+        std::string _type;
 };
 
 #endif
