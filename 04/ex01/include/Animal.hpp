e01efa3cd3d8162daf02dc04958b76d590aedf5a
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-10-04 16:40:14 +0200

    identation

--- 04/ex01/include/Animal.hpp
+++ 04/ex01/include/Animal.hpp
@@ -5,16 +5,16 @@
 
 class Animal
 {
-public:
-    Animal();
-    Animal(const Animal& other);
-    Animal& operator=(const Animal& other);
-    virtual ~Animal();
-    
-    std::string getType() const;
-    virtual void makeSound() const;
-protected:
-    std::string _type;
+    public:
+        Animal();
+        Animal(const Animal& other);
+        Animal& operator=(const Animal& other);
+        virtual ~Animal();
+        
+        std::string getType() const;
+        virtual void makeSound() const;
+    protected:
+        std::string _type;
 };
 
 #endif
