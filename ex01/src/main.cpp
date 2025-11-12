19e0e020b26fb7d659d4c06649179ce122bc0950
muffin <muffin@arch.man>
2025-11-12 13:29:30 +0100

    first try serializer, to review

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,6 +1,29 @@
 #include "Serializer.hpp"
+#include <iostream>
+#include "Data.hpp"
 
-int main(void)
-{
+int main() {
+    Data data;
+    data.id = 42;
+    data.name = "Test";
+    data.value = 24;
+
+    std::cout << "Before serialization data: " << &data << std::endl;
+
+    uintptr_t raw = Serializer::serialize(&data);
+    std::cout << "Serialized value: " << raw << std::endl;
+
+    Data* ptr = Serializer::deserialize(raw);
+    std::cout << "after deserialization data: " << ptr << std::endl;
+
+    // if (ptr == &data) {
+    //     std::cout << "✅ Success: Pointer matches!" << std::endl;
+    //     std::cout << "Data contents -> id: " << ptr->id
+    //               << ", name: " << ptr->name
+    //               << ", value: " << ptr->value << std::endl;
+    // } else {
+    //     std::cout << "❌ Error: Pointer does not match!" << std::endl;
+    // }
+
     return 0;
 }
