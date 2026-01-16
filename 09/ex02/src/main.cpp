3796b29c1a3beac170237591ad18b9ee9d95c100
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2026-01-16 15:56:45 +0100

    base for PmergeMe

---
+++ 09/ex02/src/main.cpp
@@ -0,0 +1,18 @@
+
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Usage:" << std::endl;
+    std::cout << argv[0] << " 3 5 9 7 4" << std::endl;
+    std::cout << argv[0] << " `shuf -i 1-100000 -n 3000 | tr \"\\n\" \" \"`"
+              << std::endl;
+    std::cout << argv[0] << " \"-1\" \"2\"" << std::endl;
+    std::cout << "For OSX USER:" << std::endl;
+    std::cout << argv[0] << " `jot -r 3000 1 100000 | tr \'\\n\' \' \'`"
+              << std::endl;
+    return 1;
+  }
+  return 0;
+}
